197bdb179334c8a027b71a605d8d89403b82761f
ahmadrezatabibi <ahmadreza.tabibi@gmail.com>
2014-09-19 11:33:17 -0500

    Completed models question,subject,questionOption

---
+++ src/main/java/cs544/letmegiveexam/model/Question.java
@@ -0,0 +1,103 @@
+package cs544.letmegiveexam.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ahmadreza
+ */
+@Entity
+@Table(name = "Question")
+public class Question {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToMany(mappedBy = "subject_id")
+    @Column(name = "Sub_ID", nullable = false)
+    private List<Subject> subjectLists=new ArrayList();
+    @OneToMany(mappedBy = "questionset_id")
+    @JoinTable(name="Question_QuestionSet",
+            joinColumns = @JoinColumn(name="question_Id"),
+            inverseJoinColumns = @JoinColumn(name="questionSet_Id")
+            )
+    private List<QuestionSet> questionSetLists=new ArrayList();
+    @ManyToOne
+    @JoinColumn(name = "questionoption_id")
+    @Column(name="Question_Option_ID",nullable = false)
+    private QuestionOption questionOption;
+    @Column(name="Difficulty_Level")
+    private int difficultyLevel;
+    private String question;
+    private String correctAnswer;
+
+    public Question() {
+    }
+
+    public Question(QuestionOption questionOption, int difficultyLevel, String question, String correctAnswer) {
+        this.questionOption = questionOption;
+        this.difficultyLevel = difficultyLevel;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public List<Subject> getSubjectLists() {
+        return subjectLists;
+    }
+
+    public void setSubjectLists(List<Subject> subjectLists) {
+        this.subjectLists = subjectLists;
+    }
+
+    public List<QuestionSet> getQuestionSetLists() {
+        return questionSetLists;
+    }
+
+    public void setQuestionSetLists(List<QuestionSet> questionSetLists) {
+        this.questionSetLists = questionSetLists;
+    }
+
+    public QuestionOption getQuestionOption() {
+        return questionOption;
+    }
+
+    public void setQuestionOption(QuestionOption questionOption) {
+        this.questionOption = questionOption;
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void setDifficultyLevel(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    
+    
+}
--- src/main/java/cs544/letmegiveexam/model/Subject.java
+++ src/main/java/cs544/letmegiveexam/model/Subject.java
@@ -3,6 +3,7 @@ package cs544.letmegiveexam.model;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -10,6 +11,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -34,20 +37,18 @@ public class Subject implements Serializable {
 
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    @Column(name="Question_ID",nullable = false)
+    private Question question;
+    
     public Subject() {
     }
 
-    public Subject(String name, String description) {
+    public Subject(String name, String description, Question question) {
         this.name = name;
         this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.question = question;
     }
 
     public String getName() {
@@ -65,6 +66,16 @@ public class Subject implements Serializable {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    
     
     
     
