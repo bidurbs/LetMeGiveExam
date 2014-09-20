391e4b536c1f023619a33ee1b524bb4925730012
Bidur <bidurbs@gmail.com>
2014-09-20 13:56:25 -0500

    added crud fasade

--- src/main/java/cs544/letmegiveexam/model/Subject.java
+++ src/main/java/cs544/letmegiveexam/model/Subject.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
@@ -14,6 +16,10 @@ import javax.persistence.Table;
  * @version 0.0.1
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "Subject.getAll", query = "FROM Subject s")
+
+})
 @Table(name = "Subject")
 public class Subject implements Serializable {
 
