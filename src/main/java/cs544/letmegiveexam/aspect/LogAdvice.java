ebd9bf6be52ca20643ec53ef50430acebb1c46b5
ahmadrezatabibi <ahmadreza.tabibi@gmail.com>
2014-09-23 11:51:26 -0500

    AOP for User Login

--- src/main/java/cs544/letmegiveexam/aspect/LogAdvice.java
+++ src/main/java/cs544/letmegiveexam/aspect/LogAdvice.java
@@ -6,9 +6,14 @@
 package cs544.letmegiveexam.aspect;
 
 
+import cs544.letmegiveexam.model.User;
+import java.util.Date;
+import javax.servlet.http.HttpSession;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 
 /**
  *
@@ -17,10 +22,11 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class LogAdvice {
     
-
-    @After("execution (* cs544.letmegiveexam.controller.LoginController.*(..))")
-    public void logMethodCalls(JoinPoint point) {
-            System.out.println("*******Logs... ");
+    private ILogger logger;
+    @After("execution (* cs544.letmegiveexam.controller.LoginController.welcome(..))&& args(model,session) ")
+    public void logMethodCalls(JoinPoint point, Model model,HttpSession session) {
+        User user=(User)session.getAttribute("user");
+        logger.log("username...."+user.getUsername());
 
     }
    
